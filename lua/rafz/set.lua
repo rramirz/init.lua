288095df42e0bedc909b4b5a34ddcf39adb1ec4c
Rafael Ramirez <rafi89@gmail.com>
2023-12-04 17:02:55 -0500

    see whitespaces

--- lua/rafz/set.lua
+++ lua/rafz/set.lua
@@ -26,9 +26,9 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
-
 vim.opt.updatetime = 50
-
 vim.opt.colorcolumn = "80"
 vim.opt.clipboard = "unnamedplus"
+
+vim.opt.listchars = {tab = "▸ ", space = "•"}
 
