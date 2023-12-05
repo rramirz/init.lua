fab3d40f30353509b09655338cf5c15d6bf6ede8
Rafael Ramirez <rafi89@gmail.com>
2023-12-05 08:50:00 -0500

    autocmd to update neotree when toggleterm closes

--- lua/rafz/autocmd.lua
+++ lua/rafz/autocmd.lua
@@ -7,3 +7,17 @@ vim.api.nvim_create_autocmd({"BufRead", "BufNewFile"}, {
     command = "set filetype=hcl",
 })
 
+local function refresh_neotree()
+    if vim.fn.exists("*NeoTreeShowToggle") == 1 then
+        vim.cmd("NeoTreeShowToggle")
+        vim.cmd("NeoTreeShowToggle")
+    end
+end
+
+vim.api.nvim_create_augroup("ToggleTermNeoTreeRefresh", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWinLeave", "TermClose" }, {
+    pattern = "term://*",
+    callback = refresh_neotree,
+    group = "ToggleTermNeoTreeRefresh",
+})
+
