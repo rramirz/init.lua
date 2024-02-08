9245827a3e61592fc3ef62483a4e440d7698d77b
Rafael Ramirez <rafael.ramirez@ip-192-168-1-123.eu-west-1.compute.internal>
2024-02-07 23:01:20 -0500

    updates

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,23 +1,23 @@
 local function getCurrentK8sContext()
-    local handle = io.popen("kubectl config current-context")
-    local result = handle:read("*a")
-    handle:close()
-    return result:gsub("%s+", "") -- Remove any trailing whitespace
+  local handle = io.popen("kubectl config current-context")
+  local result = handle:read("*a")
+  handle:close()
+  return result:gsub("%s+", "") -- Remove any trailing whitespace
 end
 
 local function getK8sNs()
-    local handle = io.popen("kubectl config view --minify --output 'jsonpath={..namespace}'")
-    local result = handle:read("*a")
-    handle:close()
-    return result:gsub("%s+", "") -- Remove any trailing whitespace
+  local handle = io.popen("kubectl config view --minify --output 'jsonpath={..namespace}'")
+  local result = handle:read("*a")
+  handle:close()
+  return result:gsub("%s+", "") -- Remove any trailing whitespace
 end
 
-require('lualine').setup{
+require('lualine').setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
     disabled_filetypes = {
       statusline = {
         'packer',
@@ -36,16 +36,20 @@ require('lualine').setup{
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics'},
-    lualine_c = { 'filename', getCurrentK8sContext, getK8sNs },
-    lualine_y = { 'progress'},
-    lualine_z = { 'location'}
+    lualine_b = { 'branch', 'diff', 'diagnostics' },
+    lualine_c = {
+      { 'filename', path = 1 }, -- Show relative path to the file
+      getCurrentK8sContext,
+      getK8sNs
+    },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
   },
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
-    lualine_x = {'location'},
+    lualine_c = { 'filename' },
+    lualine_x = { 'location' },
     lualine_y = {},
     lualine_z = {}
   },
--- lua/rafz/remap.lua
+++ lua/rafz/remap.lua
@@ -1,5 +1,7 @@
-
 vim.g.mapleader = " "
-  -- plus more settings as needed
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+-- plus more settings as needed
 vim.keymap.set("n", "<leader>t", vim.cmd.ToggleTerm)
+vim.keymap.set("n", "<leader>q", vim.cmd.Quit)
+vim.keymap.set('n', '<leader>x',
+  ':lua (function() local isGitRepo = vim.fn.system("git rev-parse --is-inside-work-tree"):find("true") if isGitRepo then vim.cmd("Neotree reveal_force_cwd") else vim.cmd("Neotree toggle") end end)()<CR>',
+  { noremap = true, silent = true })
