e6eba6cd6c9341e1153c0632797ca822003be418
Rafael Ramirez <rafaelramirez@Rafaels-MacBook-Pro.local>
2024-05-20 22:44:35 -0400

    remap delete

--- lua/rafz/remap.lua
+++ lua/rafz/remap.lua
@@ -2,6 +2,11 @@ vim.g.mapleader = " "
 -- plus more settings as needed
 vim.keymap.set("n", "<leader>t", vim.cmd.ToggleTerm)
 vim.keymap.set("n", "<leader>q", vim.cmd.Quit)
-vim.keymap.set('n', '<leader>x',
-  ':lua (function() local isGitRepo = vim.fn.system("git rev-parse --is-inside-work-tree"):find("true") if isGitRepo then vim.cmd("Neotree reveal_force_cwd") else vim.cmd("Neotree toggle") end end)()<CR>',
-  { noremap = true, silent = true })
+
+-- Disable clipboard for delete operations in normal mode
+vim.keymap.set('n', 'd', '"_d')
+vim.keymap.set('n', 'dd', '"_dd')
+
+-- Disable clipboard for delete operations in visual mode
+vim.keymap.set('v', 'd', '"_d')
+
