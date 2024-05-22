55217a7bd1852ff626025a21fcd215275695ad23
Rafael Ramirez <rafi89@gmail.com>
2024-05-22 09:41:10 -0400

    update delete shortcuts

--- lua/rafz/remap.lua
+++ lua/rafz/remap.lua
@@ -2,11 +2,15 @@ vim.g.mapleader = " "
 -- plus more settings as needed
 vim.keymap.set("n", "<leader>t", vim.cmd.ToggleTerm)
 vim.keymap.set("n", "<leader>q", vim.cmd.Quit)
+-- Keybinding for deleting a line without affecting the clipboard
+vim.api.nvim_set_keymap('n', '<leader>d', '"_dd', { noremap = true, silent = true })
 
--- Disable clipboard for delete operations in normal mode
-vim.keymap.set('n', 'd', '"_d')
-vim.keymap.set('n', 'dd', '"_dd')
+-- Keybinding for deleting a word without affecting the clipboard
+vim.api.nvim_set_keymap('n', '<leader>w', '"_dw', { noremap = true, silent = true })
 
--- Disable clipboard for delete operations in visual mode
-vim.keymap.set('v', 'd', '"_d')
+-- Keybinding for changing a line without affecting the clipboard
+vim.api.nvim_set_keymap('n', '<leader>c', '"_cc', { noremap = true, silent = true })
 
+-- Keybinding for visual selection delete without affecting the clipboard
+vim.api.nvim_set_keymap('v', '<leader>d', '"_d', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<leader>d', '"_dG', { noremap = true, silent = true })
